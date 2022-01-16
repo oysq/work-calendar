cbc307dc097bb093c5d066ad7831015c605bf4d1
oysq <1585228683@qq.com>
2022-01-16 17:55:33 +0800

    zl

--- js/calendar/calendar.js
+++ js/calendar/calendar.js
@@ -8,8 +8,9 @@ Vue.use(vant.Divider);
 
 
 Vue.prototype.$axios = axios
+Vue.prototype.$axios.defaults.baseURL = 'http://excel.oysq.cloud/calendar-ms'
 // Vue.prototype.$axios.defaults.baseURL = 'http://81.71.39.253:80/calendar-ms'
-Vue.prototype.$axios.defaults.baseURL = 'http://127.0.0.1:80/calendar-ms'
+// Vue.prototype.$axios.defaults.baseURL = 'http://127.0.0.1:80/calendar-ms'
 
 // 吐司
 Vue.prototype.$toast = vant.Toast
@@ -34,7 +35,8 @@ new Vue({
 				name: "",
 				password: "",
 				id: "",
-				token: ""
+				token: "",
+				postSalary: 0
 			},
 			// 打卡弹窗
 			punchPopup: {
@@ -44,17 +46,92 @@ new Vue({
 				timeConfirm: ""
 			},
 			// 日历
-			showCalendar: false,
-			minDate: new Date("2021/06/01"),
 			calendar: {
-				selectDate: new Date()
+				show: false,
+				selectDate: new Date(),
+				minDate: new Date("2021/06/01")
 			},
 			// 当月打卡记录
-			punchRecord: {}
+			punchRecord: {},
+			// 打卡详情
+			detail: {
+				show: false,
+				startTime: "",
+				endTime: "",
+				overtimeDuration: "",
+				overtimePay: ""
+			},
+			// 工具盒子
+			toolBox: {
+				show: false,
+				actions: [
+					{
+						name: '开始时间',
+						code: "start"
+					},
+					{
+						name: '节假倍率',
+						code: "rate"
+					},
+					{
+						name: '岗位工资',
+						code: "salary"
+					}
+				]
+			},
+			// 调整开始时间弹窗
+			startConf: {
+				show: false,
+				title: "",
+				timeSelect: "",
+				timeConfirm: ""
+			},
+			// 倍率调整
+			rateAction: {
+				show: false,
+				defaultIndex: 0,
+				columns: [
+					{
+						name: '1.5倍',
+						value: 1.5
+					},
+					{
+						name: '2倍',
+						value: 2.0
+					},
+					{
+						name: '3倍',
+						value: 3.0
+					}
+				]
+			},
+			// 岗位工资
+			salaryConf: {
+				show: false,
+				postSalary: 0
+			}
 		}
 	},
 	created() {
 		this.checkToken();
+		setTimeout(() => {
+
+			if (navigator.userAgent.toLocaleLowerCase().includes('iphone')) {
+				// @ts-ignore
+				this.intervalFlag = setInterval(() => {
+					document.querySelector('body').scrollTop = 0;
+				}, 200);
+			}
+	      
+			if (navigator.userAgent.toLowerCase().includes('iphone')) {
+				if(document.querySelector('app-confirm-loan')) {
+					document.querySelector('app-confirm-loan').addEventListener('touchmove', (e) => {
+						e.preventDefault();
+					});
+				}
+			}
+
+	    }, 100);
 	},
 	methods: {
 		/**
@@ -77,6 +154,7 @@ new Vue({
 					this.hideLoding();
 			    	if(res.data.status == 1) {
 			    		this.user.id = res.data.body.userId
+			    		this.user.postSalary = res.data.body.postSalary
 				   	    this.$notify({ type: 'success', message: "身份验证通过，欢迎 " + res.data.body.userName});
 				   	    // 加载数据
 				   	    this.queryCalendarList(this.calendar.selectDate);
@@ -163,6 +241,7 @@ new Vue({
 			).then(res => {
 				if(res.data.status == 1) {
 					this.user.id = res.data.body.userId
+					this.user.postSalary = res.data.body.postSalary
 					window.localStorage.setItem("authToken", res.data.body.token);
 					this.showAuthPopup = false;
 					this.$notify({ type: 'success', message: "身份验证通过，欢迎 " + res.data.body.userName});
@@ -197,7 +276,9 @@ new Vue({
 					console.log("=== punchRecord ===");
 					console.log(this.punchRecord);
 					// 开始加载渲染数据
-					this.showCalendar = true;
+					this.calendar.show = true;
+					// 首次舒心时渲染一次
+					this.formatterDetail(new Date());
 				} else {
 					this.$toast.fail(res.data.msg);
 				}
@@ -229,6 +310,20 @@ new Vue({
 			return day;
 
 		},
+		// 渲染详情信息
+		formatterDetail(date) {
+			const record = this.findRecord(date);
+			if(record) {
+				this.detail.show = true;
+				this.detail.startTime = record.startTime.substring(5, 16).replace("/","-");
+				this.detail.endTime = record.endTime.substring(5, 16).replace("/","-");
+				this.detail.overtimeDuration = record.overtimeDuration;
+				this.detail.multiplyRate = record.multiplyRate;
+				this.detail.overtimePay = record.overtimePay;
+			} else {
+				this.detail.show = false;
+			}
+		},
 		// 某个月进入可视区域
 		monthShow(obj) {
 			console.log("进入可视区域: " + this.formatDate(obj.date, "-"))
@@ -237,13 +332,13 @@ new Vue({
 		selectCalendar(clickDate) {
 			this.calendar.selectDate = clickDate
 			console.log("点击日期: " + this.formatDate(clickDate, "-"));
+			this.formatterDetail(this.calendar.selectDate);
 		},
 		/**
 		 * 打卡
 		 */
 		punchClick() {
 			this.punchPopup.title = this.formatDate(this.calendar.selectDate, "-");
-			// TODO 有打卡的要切换到对应的时间
 			// 调整当前时间选择器的时间
 			const record = this.findRecord(this.calendar.selectDate);
 			if(record) {
@@ -264,13 +359,19 @@ new Vue({
 			this.punchPopup.timeConfirm = this.punchPopup.timeSelect;
 			console.log("timeConfirm: " + this.punchPopup.timeConfirm);
 
+			// 早上 08:30 之前的算前一天
+			var punchDate = new Date(this.calendar.selectDate);
+			if(this.punchPopup.timeConfirm.split(":")[0] <= "08" && this.punchPopup.timeConfirm.split(":")[1] < "30") {
+				punchDate.setDate(punchDate.getDate() - 1)
+			}
+
 			this.punchPopup.show = false;
 			
 			this.showLoding();
 			this.$axios.post('/punchRecord/updateEndTime',
 				{
 					userId: this.user.id,
-					punchDate: this.formatDate(this.calendar.selectDate, "/"),
+					punchDate: this.formatDate(punchDate, "/"),
 					endTime: this.formatDate(this.calendar.selectDate, "/") + " " + this.punchPopup.timeConfirm + ":00"
 				}
 			).then(res => {
@@ -285,6 +386,164 @@ new Vue({
 				console.log(error);
 			    alert(error);
 			});
+		},
+		/**
+		 * 工具盒子
+		 */
+		toolBoxClick() {
+			this.toolBox.show = true;
+		},
+		toolBoxSelect(item) {
+			
+			this.toolBox.show = false;
+
+			// 开始时间
+			if(item.code == "start") {
+				this.startConfClick();
+			}
+
+			// 倍率
+			if(item.code == "rate") {
+				this.rateActionShow();
+			}
+
+			// 岗位薪资
+			if(item.code == "salary") {
+				this.salaryShow();
+			}
+		},
+		/**
+		 * 调整开始时间
+		 */
+		startConfClick() {
+			this.startConf.title = this.formatDate(this.calendar.selectDate, "-");
+			// 调整当前时间选择器的时间
+			const record = this.findRecord(this.calendar.selectDate);
+			if(record) {
+				if(record['startTime']) {
+					this.startConf.timeSelect = record['startTime'].split(" ")[1].substring(0, 5);
+				}
+			} else {
+				this.startConf.timeSelect = "18:00";
+			}
+			this.startConf.show = true;
+		},
+		startConfConfirm() {
+
+			this.startConf.timeConfirm = this.startConf.timeSelect;
+			console.log("timeConfirm: " + this.startConf.timeConfirm);
+
+			// 早上 08:30 之前的算前一天
+			var punchDate = new Date(this.calendar.selectDate);
+			if(this.startConf.timeConfirm.split(":")[0] <= "08" && this.startConf.timeConfirm.split(":")[1] < "30") {
+				startConf.setDate(punchDate.getDate() - 1)
+			}
+			console.log(punchDate)
+
+			this.startConf.show = false;
+			
+			this.showLoding();
+			this.$axios.post('/punchRecord/updateStartTime',
+				{
+					userId: this.user.id,
+					punchDate: this.formatDate(punchDate, "/"),
+					startTime: this.formatDate(this.calendar.selectDate, "/") + " " + this.startConf.timeConfirm + ":00"
+				}
+			).then(res => {
+				this.hideLoding();
+				if(res.data.status == 1) {
+					this.$notify({ type: 'success', message: "调整成功"});
+					setTimeout("window.location.reload()", 1000);
+				} else {
+					this.$toast.fail(res.data.msg);
+				}
+			}).catch(function (error) {
+				console.log(error);
+			    alert(error);
+			});
+		},
+		/**
+		 * 倍率调整
+		 */
+		// 设置默认值并弹出
+		rateActionShow() {
+			// 设置默认值
+			const record = this.findRecord(this.calendar.selectDate);
+			if(record) {
+				if(record['multiplyRate']) {
+					if(record['multiplyRate'] == 1.5) {
+						this.rateAction.defaultIndex = 0;
+					} else if (record['multiplyRate'] == 2.0) {
+						this.rateAction.defaultIndex = 1;
+					} else if (record['multiplyRate'] == 3.0) {
+						this.rateAction.defaultIndex = 2;
+					} else {
+						this.rateAction.defaultIndex = 0;
+					}
+				}
+			}
+			this.rateAction.show = true;
+		},
+		// 倍率确认
+		rateActionConfirm(item) {
+
+			this.rateAction.show = false;
+			
+			this.showLoding();
+			this.$axios.post('/punchRecord/updateMultiplyRate',
+				{
+					userId: this.user.id,
+					punchDate: this.formatDate(this.calendar.selectDate, "/"),
+					multiplyRate: item.value
+				}
+			).then(res => {
+				this.hideLoding();
+				if(res.data.status == 1) {
+					this.$notify({ type: 'success', message: "调整成功"});
+					setTimeout("window.location.reload()", 1000);
+				} else {
+					this.$toast.fail(res.data.msg);
+				}
+			}).catch(function (error) {
+				console.log(error);
+			    alert(error);
+			});
+		},
+		/**
+		 * 岗位工资调整
+		 */
+		salaryShow() {
+
+			if(this.user.postSalary) {
+				this.salaryConf.postSalary = this.user.postSalary;
+			}
+
+			this.salaryConf.show = true;
+
+		},
+		salaryConfirm() {
+
+			this.salaryConf.show = false;
+
+			this.showLoding();
+			this.$axios.post('/user/updatePostSalary',
+				{
+					userId: this.user.id,
+					postSalary: this.salaryConf.postSalary
+				}
+			).then(res => {
+				this.hideLoding();
+				if(res.data.status == 1) {
+					this.$notify({ type: 'success', message: "设置成功"});
+					setTimeout("window.location.reload()", 1000);
+				} else {
+					this.$toast.fail(res.data.msg);
+				}
+			}).catch(function (error) {
+				console.log(error);
+			    alert(error);
+			});
+
 		},
 		/**
 		 * 报表
